b9d074f567b0e2323048b2d5d464ba78d296299b
wangsongyan <1104237534@qq.com>
2016-07-07 10:41:06 +0800

    添加跳转链接处理

--- src/main/java/org/apache/cordova/CustomerPlugin.java
+++ src/main/java/org/apache/cordova/CustomerPlugin.java
@@ -1,18 +1,23 @@
 package org.apache.cordova;
 
+import android.net.Uri;
+
 /**
  * Created by songy on 2016/7/6.
  */
 public class CustomerPlugin extends CordovaPlugin {
 
+  //允许js访问原生代码
   @Override
   public Boolean shouldAllowBridgeAccess(String url) {
     return true;
   }
 
-//  @Override
-//  public boolean onOverrideUrlLoading(String url) {
-//    return true;
-//  }
+  //跳转链接处理，点击链接跳转页面
+  @Override
+  public boolean onOverrideUrlLoading(String url) {
+    this.webView.loadUrl(url);
+    return true;
+  }
 
 }
